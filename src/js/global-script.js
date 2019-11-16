1539105834148122a6e68d0d6f0c3f0da6ba26f0
amstelart <amsteldroid@gmail.com>
2019-11-16 23:00:36 +0300

    all

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -6,9 +6,10 @@ $( document ).ready(function() {
       loop: true,
       center: true,
       margin: 30,
+      navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
       responsive : {
         0 : {
-          items: 1,
+          items: 2,
           nav: true,
           loop: true,
           center: true,
@@ -17,10 +18,10 @@ $( document ).ready(function() {
           items: 3,
         },
         768 : {
-          items: 3,
+          items: 4,
         },
         992 : {
-          items: 3,
+          items: 5,
         },
         1200 : {
           items: 5,
